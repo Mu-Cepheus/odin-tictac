714deabc0ca2e2ad113db64f10958159f85461ff
Mu-Cepheus <>
2024-09-02 12:17:24 +0800

    Convert to ES6 class syntax

--- script.js
+++ script.js
@@ -1,92 +1,141 @@
-//player objects storing player name? and player marker
-function createPlayer(argName, argMarker) {
-    const name = argName
-    const marker = argMarker;
+class Player {
+    #name;
+    #marker;
+    constructor(argname, argMarker) {
+        this.#name = argname;
+        this.#marker = argMarker;
+    }
 
-    return { "name": argName, "marker": argMarker };
+    get name() {
+        return this.#name;
+    }
+
+    set name(argName) {
+        this.#name = argName;
+    }
+
+    get marker() {
+        return this.#marker;
+    }
 }
 
 let boardChildren = document.querySelector(".board").children;
 let board = document.querySelector(".board");
-let player1 = createPlayer("Player 1", "X");
-let player2 = createPlayer("Player 2", "O");
+let player1 = new Player("Player 1", "X");
+let player2 = new Player("Player 2", "O");
 let statusText = document.querySelector(".status>p");
 let resetButton = document.querySelector("#reset");
 let p1Button = document.querySelector("#p1Button");
 let p2Button = document.querySelector("#p2Button");
 
-const gameBoard = (function () {
-    let board = [[null, null, null], [null, null, null], [null, null, null]]
-    let lastMarker;
+class GameBoard {
+    static #board = [[null, null, null], [null, null, null], [null, null, null]];
+    static #lastMarker;
 
-    function getLastMarker() {
-        return lastMarker;
+    static get lastMarker() {
+        return this.#lastMarker;
     }
 
-    function checkWin(argRow, argCol, argMarker) {
-        switch (argRow) {
+    static get board() {
+        for (let i = 0; i < this.#board.length; i++) {
+            console.log(`[${this.#board[i][0]}] [${this.#board[i][1]}] [${this.#board[i][2]}]`);
+        }
+        return this.#board.flat(Infinity);
+    }
+
+    static setBoardSpace(argData) {
+        if (!(this.#board.some((row) => { return row.includes("O") || row.includes("X") })) && argData.marker !== "X") {
+            throw new Error("First input must be X!");
+        } else if (argData.row > 2 || argData.row < 0 || argData.column > 2 || argData.column < 0) {
+            throw new Error("Invalid coordinates!")
+        } else if (argData.marker !== "X" && argData.marker !== "O") {
+            throw new Error("Invalid marker!");
+        } else if (this.#lastMarker === argData.marker) {
+            throw new Error("It is not your turn!")
+        } else if (this.#board[argData.row][argData.column] !== null) {
+            throw new Error("That space is taken!")
+        } else {
+            this.#board[argData.row][argData.column] = argData.marker;
+            if (this.#board.every((row) => !row.includes(null))) {
+                this.board;
+                return { winStatus: "full", winMarker: null };
+            } else {
+                const winning = this.#checkWin(argData);
+                this.#lastMarker = argData.marker;
+                this.board;
+                return winning;
+            }
+        }
+    }
+
+    static resetBoard() {
+        this.#board = [[null, null, null], [null, null, null], [null, null, null]];
+    }
+
+    static #checkWin(argData) {
+        switch (argData.row) {
             case 0:
-                switch (argCol) {
+                switch (argData.column) {
                     case 0:
-                        if ((argMarker === board[0][1] && argMarker === board[0][2]) || (argMarker === board[1][0] && argMarker === board[2][0]) || (argMarker === board[1][1] && argMarker === board[2][2])) {
-                            console.log(`${argMarker} wins`)
-                            return { winStatus: true, winMarker: argMarker };
+                        if ((argData.marker === this.#board[0][1] && argData.marker === this.#board[0][2]) || (argData.marker === this.#board[1][0] && argData.marker === this.#board[2][0]) || (argData.marker === this.#board[1][1] && argData.marker === this.#board[2][2])) {
+                            console.log(`${argData.marker} wins`)
+                            return { winStatus: true, winMarker: argData.marker };
                         }
                         break;
                     case 1:
-                        if ((argMarker === board[0][0] && argMarker === board[0][2]) || (argMarker === board[1][1] && argMarker === board[2][1])) {
-                            console.log(`${argMarker} wins`)
-                            return { winStatus: true, winMarker: argMarker };
+                        if ((argData.marker === this.#board[0][0] && argData.marker === this.#board[0][2]) || (argData.marker === this.#board[1][1] && argData.marker === this.#board[2][1])) {
+                            console.log(`${argData.marker} wins`)
+                            return { winStatus: true, winMarker: argData.marker };
                         }
                         break;
                     case 2:
-                        if ((argMarker === board[0][1] && argMarker === board[0][0]) || (argMarker === board[1][2] && argMarker === board[2][2]) || (argMarker === board[1][1] && argMarker === board[2][0])) {
-                            console.log(`${argMarker} wins`)
-                            return { winStatus: true, winMarker: argMarker };
+                        if ((argData.marker === this.#board[0][1] && argData.marker === this.#board[0][0]) || (argData.marker === this.#board[1][2] && argData.marker === this.#board[2][2]) || (argData.marker === this.#board[1][1] && argData.marker === this.#board[2][0])) {
+                            console.log(`${argData.marker} wins`)
+                            return { winStatus: true, winMarker: argData.marker };
                         }
                         break;
                 }
                 break;
             case 1:
-                switch (argCol) {
+                switch (argData.column) {
                     case 0:
-                        if ((argMarker === board[0][0] && argMarker === board[2][0]) || (argMarker === board[1][1] && argMarker === board[1][2])) {
-                            console.log(`${argMarker} wins`)
-                            return { winStatus: true, winMarker: argMarker };
+                        if ((argData.marker === this.#board[0][0] && argData.marker === this.#board[2][0]) || (argData.marker === this.#board[1][1] && argData.marker === this.#board[1][2])) {
+                            console.log(`${argData.marker} wins`)
+                            return { winStatus: true, winMarker: argData.marker };
                         }
                         break;
                     case 1:
-                        if ((argMarker === board[0][1] && argMarker === board[2][1]) || (argMarker === board[1][0] && argMarker === board[1][2]) || (argMarker === board[0][0] && argMarker === board[2][2]) || (argMarker === board[0][2] && argMarker === board[2][0])) {
-                            console.log(`${argMarker} wins`)
-                            return { winStatus: true, winMarker: argMarker };
+                        if ((argData.marker === this.#board[0][1] && argData.marker === this.#board[2][1]) || (argData.marker === this.#board[1][0] && argData.marker === this.#board[1][2]) || (argData.marker === this.#board[0][0] && argData.marker === this.#board[2][2]) || (argData.marker === this.#board[0][2] && argData.marker === this.#board[2][0])) {
+                            console.log(`${argData.marker} wins`)
+                            return { winStatus: true, winMarker: argData.marker };
                         }
                         break;
                     case 2:
-                        if ((argMarker === board[0][2] && argMarker === board[2][2]) || (argMarker === board[1][0] && argMarker === board[1][1])) {
-                            console.log(`${argMarker} wins`)
-                            return { winStatus: true, winMarker: argMarker };
+                        if ((argData.marker === this.#board[0][2] && argData.marker === this.#board[2][2]) || (argData.marker === this.#board[1][0] && argData.marker === this.#board[1][1])) {
+                            console.log(`${argData.marker} wins`)
+                            return { winStatus: true, winMarker: argData.marker };
                         }
                         break;
                 }
                 break;
             case 2:
-                switch (argCol) {
+                switch (argData.column) {
                     case 0:
-                        if ((argMarker === board[0][0] && argMarker === board[1][0]) || (argMarker === board[2][1] && argMarker === board[2][2]) || (argMarker === board[1][1] && argMarker === board[0][2])) {
-                            console.log(`${argMarker} wins`)
-                            return { winStatus: true, winMarker: argMarker };
+                        if ((argData.marker === this.#board[0][0] && argData.marker === this.#board[1][0]) || (argData.marker === this.#board[2][1] && argData.marker === this.#board[2][2]) || (argData.marker === this.#board[1][1] && argData.marker === this.#board[0][2])) {
+                            console.log(`${argData.marker} wins`)
+                            return { winStatus: true, winMarker: argData.marker };
                         }
                         break;
                     case 1:
-                        if ((argMarker === board[2][0] && argMarker === board[2][2]) || (argMarker === board[0][1] && argMarker === board[1][1])) {
-                            console.log(`${argMarker} wins`)
-                            return { winStatus: true, winMarker: argMarker };
+                        if ((argData.marker === this.#board[2][0] && argData.marker === this.#board[2][2]) || (argData.marker === this.#board[0][1] && argData.marker === this.#board[1][1])) {
+                            console.log(`${argData.marker} wins`)
+                            return { winStatus: true, winMarker: argData.marker };
                         }
                         break;
                     case 2:
-                        if ((argMarker === board[0][2] && argMarker === board[1][2]) || (argMarker === board[2][0] && argMarker === board[2][1]) || (argMarker === board[0][0] && argMarker === board[1][1])) {
-                            console.log(`${argMarker} wins`)
-                            return { winStatus: true, winMarker: argMarker };
+                        if ((argData.marker === this.#board[0][2] && argData.marker === this.#board[1][2]) || (argData.marker === this.#board[2][0] && argData.marker === this.#board[2][1]) || (argData.marker === this.#board[0][0] && argData.marker === this.#board[1][1])) {
+                            console.log(`${argData.marker} wins`)
+                            return { winStatus: true, winMarker: argData.marker };
                         }
                         break;
                 }
@@ -94,64 +143,26 @@ const gameBoard = (function () {
         }
         return { winStatus: false, winMarker: null };
     }
+}
 
-    const getBoard = function () {
-        for (let i = 0; i < board.length; i++) {
-            console.log(`[${board[i][0]}] [${board[i][1]}] [${board[i][2]}]`);
-        }
-        return board.flat(Infinity);
-    };
-
-    const setBoard = function (argRow, argCol, argMarker) {
-        if (!(board.some((row) => { return row.includes("O") || row.includes("X") })) && argMarker !== "X") {
-            throw new Error("First input must be X!");
-        } else if (argRow > 2 || argRow < 0 || argCol > 2 || argCol < 0) {
-            throw new Error("Invalid coordinates!")
-        } else if (argMarker !== "X" && argMarker !== "O") {
-            throw new Error("Invalid marker!");
-        } else if (lastMarker === argMarker) {
-            throw new Error("It is not your turn!")
-        } else if (board[argRow][argCol] !== null) {
-            throw new Error("That space is taken!")
-        } else {
-            board[argRow][argCol] = argMarker;
-            if (board.every((row) => !row.includes(null))) {
-                getBoard();
-                return { winStatus: "full", winMarker: null };
-            } else {
-                const winning = checkWin(argRow, argCol, argMarker);
-                lastMarker = argMarker;
-                getBoard();
-                return winning;
-            }
-        }
-    }
-
-    const resetBoard = function () {
-        board = [[null, null, null], [null, null, null], [null, null, null]];
-    }
-
-    return { getBoard, setBoard, resetBoard, getLastMarker }
-})();
-
-const displayController = (function () {
-    const loadBoard = function () {
-        let board = gameBoard.getBoard();
+class DisplayController {
+    static loadBoard() {
+        let board = GameBoard.board;
         for (let i = 0; i < board.length; i++) {
             if (board[i]) {
                 boardChildren[i].textContent = board[i];
             }
         }
-    };
+    }
 
-    const resetButton = function () {
-        gameBoard.resetBoard();
+    static resetButton() {
+        GameBoard.resetBoard();
         for (const element of boardChildren) {
             element.textContent = "";
         }
-    };
+    }
 
-    const nameChange = function (event) {
+    static nameChange(event) {
         event.preventDefault();
         let textBox;
         if (event.target.id === "p1Button") {
@@ -163,9 +174,9 @@ const displayController = (function () {
         }
     }
 
-    const placeMarker = function (event) {
+    static placeMarker(event) {
         let target = event.target;
-        let lastMarker = gameBoard.getLastMarker();
+        let lastMarker = GameBoard.lastMarker;
         let currentMarker, winMarker;
         let winStatus = false;
         if ((!lastMarker || lastMarker === "O") && (event.target.textContent !== "X" || event.target.textContent !== "O")) {
@@ -179,31 +190,31 @@ const displayController = (function () {
         }
         switch (target.id) {
             case "space1":
-                ({ winStatus, winMarker } = gameBoard.setBoard(0, 0, currentMarker));
+                ({ winStatus, winMarker } = GameBoard.setBoardSpace({ row: 0, column: 0, marker: currentMarker }));
                 break;
             case "space2":
-                ({ winStatus, winMarker } = gameBoard.setBoard(0, 1, currentMarker));
+                ({ winStatus, winMarker } = GameBoard.setBoardSpace({ row: 0, column: 1, marker: currentMarker }));
                 break;
             case "space3":
-                ({ winStatus, winMarker } = gameBoard.setBoard(0, 2, currentMarker));
+                ({ winStatus, winMarker } = GameBoard.setBoardSpace({ row: 0, column: 2, marker: currentMarker }));
                 break;
             case "space4":
-                ({ winStatus, winMarker } = gameBoard.setBoard(1, 0, currentMarker));
+                ({ winStatus, winMarker } = GameBoard.setBoardSpace({ row: 1, column: 0, marker: currentMarker }));
                 break;
             case "space5":
-                ({ winStatus, winMarker } = gameBoard.setBoard(1, 1, currentMarker));
+                ({ winStatus, winMarker } = GameBoard.setBoardSpace({ row: 1, column: 1, marker: currentMarker }));
                 break;
             case "space6":
-                ({ winStatus, winMarker } = gameBoard.setBoard(1, 2, currentMarker));
+                ({ winStatus, winMarker } = GameBoard.setBoardSpace({ row: 1, column: 2, marker: currentMarker }));
                 break;
             case "space7":
-                ({ winStatus, winMarker } = gameBoard.setBoard(2, 0, currentMarker));
+                ({ winStatus, winMarker } = GameBoard.setBoardSpace({ row: 2, column: 0, marker: currentMarker }));
                 break;
             case "space8":
-                ({ winStatus, winMarker } = gameBoard.setBoard(2, 1, currentMarker));
+                ({ winStatus, winMarker } = GameBoard.setBoardSpace({ row: 2, column: 1, marker: currentMarker }));
                 break;
             case "space9":
-                ({ winStatus, winMarker } = gameBoard.setBoard(2, 2, currentMarker));
+                ({ winStatus, winMarker } = GameBoard.setBoardSpace({ row: 2, column: 2, marker: currentMarker }));
                 break;
         }
         if (winStatus === true && winMarker) {
@@ -215,12 +226,10 @@ const displayController = (function () {
         } else if (winStatus === "full") {
             statusText.textContent = `board is full`;
         }
-    };
+    }
+}
 
-    return { loadBoard, placeMarker, resetButton, nameChange }
-})();
-
-board.addEventListener("click", displayController.placeMarker)
-resetButton.addEventListener("click", displayController.resetButton)
-p1Button.addEventListener("click", displayController.nameChange)
-p2Button.addEventListener("click", displayController.nameChange)
+board.addEventListener("click", DisplayController.placeMarker)
+resetButton.addEventListener("click", DisplayController.resetButton)
+p1Button.addEventListener("click", DisplayController.nameChange)
+p2Button.addEventListener("click", DisplayController.nameChange)
